48e8b3aaf432f59bcf20c4a6a0b08559c1d7faaf
Sonu Rajak <sonu9142545236@gmail.com>
2025-01-11 11:07:45 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// mongodb+srv://sonu7:12345@cluster0.vywnm.mongodb.net/?
+
+
+// App Config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// DB Connection 
+connectDB();
+
+// API endpoints
+app.use("/api/food", foodRouter);                                // use this endpoint + '/type' (methods in foodRouter.js)
+app.use("/images",express.static('uploads'));                // When we add any image in uploads folder we can access it in 'localhost:4000/images/filename' endpoint
+app.use("/api/user", userRouter);
+app.use("/api/cart",cartRouter);
+app.use("/api/order", orderRouter);
+
+// Route
+app.get("/", (req,res) => {
+    res.send("API Working")
+})
+
+// run server
+app.listen(port, ()=>{
+    console.log(`Server started on http://localhost:${port} `)
+})
+
+
+
+//0.0.0.0/0  (includes your current IP address) ACCESS DATABASE FROM ANY IP ADDRESS
